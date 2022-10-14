b51c0e84dc6efe33b304baae67ea0ebf634d1fbb
Francis Imperial Wong <franciswong17@gmail.com>
2022-10-14 19:40:23 +0000

    moved staging back one folder

---
+++ greenery/models/staging/greenery/_stg__users.sql
@@ -0,0 +1,25 @@
+{{
+  config(
+    materialized = 'view'
+    , unique_key = 'user_guid'
+  )
+}}
+
+with users_source as (
+  select * from {{ source('src_greenery', 'users') }}
+)
+
+, renamed_casted as (
+  select 
+    user_id as user_guid
+    , first_name
+    , last_name
+    , email
+    , phone_number
+    , created_at as created_at_utc
+    , updated_at as updated_at_utc
+    , address_id as address_guid
+  from users_source
+)
+
+select * from renamed_casted
